da7cd7c9403266885436cae014df3449e1304021
Kubra Hanedar <the.haneda@gmail.com>
2022-11-20 16:18:01 +0300

    tanımlamalar yapıldı

--- CatchTheKennyGame/ViewController.swift
+++ CatchTheKennyGame/ViewController.swift
@@ -8,6 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    
+    
+    //Views
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var highscoreLabel: UILabel!
+    @IBOutlet weak var kenny1: UIImageView!
+    @IBOutlet weak var kenny2: UIImageView!
+    @IBOutlet weak var kenny3: UIImageView!
+    @IBOutlet weak var kenny4: UIImageView!
+    @IBOutlet weak var kenny5: UIImageView!
+    @IBOutlet weak var kenny6: UIImageView!
+    @IBOutlet weak var kenny7: UIImageView!
+    @IBOutlet weak var kenny8: UIImageView!
+    @IBOutlet weak var kenny9: UIImageView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,5 +33,8 @@ class ViewController: UIViewController {
     }
 
 
+    
+    
+    
 }
 
